df03c5be65f853eda6baed312d141c45b4148777
Juaan26 <Juaan26g@gmail.com>
2025-07-04 17:11:05 +0200

    creación de un componente

--- src/main.jsx
+++ src/main.jsx
@@ -2,4 +2,16 @@
 import { createRoot } from "react-dom/client"
 
 const root = createRoot(document.querySelector('#app'))
-root.render(<h1>"Hello, World!"</h1>)
+
+
+function MainContent() {
+    return <h1>React is great</h1>
+}
+
+
+
+root.render(<>
+    <MainContent />
+    <h1>"Hello, World!"</h1>
+</>
+)
