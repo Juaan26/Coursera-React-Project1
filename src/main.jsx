accb6d66aa3fec6fbc1dd7120e018e58445889a1
Juaan26 <Juaan26g@gmail.com>
2025-07-04 22:18:37 +0200

    creada estructura html basica del proyecto

--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,22 @@ import { createRoot } from "react-dom/client"
 const root = createRoot(document.querySelector('#app'))
 
 
-function MainContent() {
-    return <h1>React is great</h1>
+
+function Logo() {
+    return <img src="vite.svg" alt="Vite logo" width="40px" />
 }
 
 
 
 root.render(<>
-    <MainContent />
-    <h1>"Hello, World!"</h1>
+    <Logo />
+    <h1>Fun facts about React</h1>
+    <ul>
+        <li>Was first release in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has well over 200K stars on GitHub</li>
+        <li>Is maintained by Meta</li>
+        <li>Powers thousands of enterprise apps, including mobile apps</li>
+    </ul>
 </>
 )
