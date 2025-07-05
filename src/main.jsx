3845b8b9c8930f9e848cf67230865121c8a6fd21
Juaan26 <Juaan26g@gmail.com>
2025-07-05 19:56:48 +0200

    creación de componentes personalizados

--- src/main.jsx
+++ src/main.jsx
@@ -9,17 +9,26 @@ function Logo() {
     return <img src="vite.svg" alt="Vite logo" width="40px" />
 }
 
+function ReactFacts() {
+    return (
+        <>
+            <h1>Fun facts about React</h1>
+            <ul>
+                <li>Was first release in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 200K stars on GitHub</li>
+                <li>Is maintained by Meta</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
+        </>)
+}
+
+console.log(<h1>Fun facts about React</h1>)
+console.log("<h1>Fun facts about React</h1>")
 
 
 root.render(<>
     <Logo />
-    <h1>Fun facts about React</h1>
-    <ul>
-        <li>Was first release in 2013</li>
-        <li>Was originally created by Jordan Walke</li>
-        <li>Has well over 200K stars on GitHub</li>
-        <li>Is maintained by Meta</li>
-        <li>Powers thousands of enterprise apps, including mobile apps</li>
-    </ul>
+    <ReactFacts />
 </>
 )
