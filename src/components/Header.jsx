ea2cf352433f0ae60a4fb2a6033cc1dd3602eecf
Juaan26 <Juaan26g@gmail.com>
2025-07-06 02:22:25 +0200

    terminado el proyecto 1

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,9 @@
 export default function Header() {
     return (
         <header className="header">
-            <img src="vite.svg" alt="Vite logo" width="40px" />
             <nav className="header-nav">
-                <ul className="header-nav-items">
-                    <li>Pricing</li>
-                    <li>About</li>
-                    <li>Contact</li>
-                </ul>
+                <img src="react.svg" alt="React logo" width="40px" />
+                <span>ReactFacts</span>
             </nav>
         </header>
     )
